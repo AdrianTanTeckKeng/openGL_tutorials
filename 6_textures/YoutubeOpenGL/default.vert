b156e7a58c493bfb8d98f6f111b0c3fe6099046d
atanteck <atanteck@caltech.edu>
2021-08-11 12:35:23 -0600

    Added stb_image.h to read in files for texturing.

--- 6_textures/YoutubeOpenGL/default.vert
+++ 6_textures/YoutubeOpenGL/default.vert
@@ -3,10 +3,9 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 color;
-uniform float scale;
 
 void main()
 {
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0f);
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
 	color = aColor;
 };
--- 6_textures/YoutubeOpenGL/main.cpp
+++ 6_textures/YoutubeOpenGL/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
@@ -42,19 +43,16 @@ int main()
 	GLfloat vertices[] =
 	{
 		// Coordinates // Colors //
-		-0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f, 0.02f,
-		 0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f, 0.02f,
-		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.32f,
-		-0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f,
-		 0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f, 0.9f, 0.45f, 0.17f,
-		 0.0f, -0.5f * float(sqrt(3)) / 3,     0.0f, 0.8f, 0.3f,  0.02f
+		-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
+		 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,		1.0f, 1.0f, 1.0f
 	};
 
 	GLuint indices[] =
 	{
-		0, 3, 5,
-		3, 2, 4,
-		5, 4, 1
+		0, 2, 1,
+		0, 3, 2
 	};
 
 	// Next, set up VAO and VBO
@@ -73,9 +71,6 @@ int main()
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-
-
 
 	// Set up color buffer
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -87,9 +82,8 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		shaderProgram.Activate();
-		glUniform1f(uniID, 0.5);
-		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		VAO1.Bind(); 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
