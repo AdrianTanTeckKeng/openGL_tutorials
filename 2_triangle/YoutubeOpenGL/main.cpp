ab4bdf6ad6ea81f9e19db6a2bfe17f1ea8108dab
atanteck <atanteck@caltech.edu>
2021-08-14 13:33:22 -0700

    Testing SSH.

--- 2_triangle/YoutubeOpenGL/main.cpp
+++ 2_triangle/YoutubeOpenGL/main.cpp
@@ -5,6 +5,7 @@
 // Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
@@ -20,105 +21,58 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 int main()
 {
-	// Initialize GLFW(the program that creates window and process inputs)
-	glfwInit();
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Youtube", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    GLfloat triangle1Vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    GLfloat triangle2Vertices[] = {
+        0.f, -0.5f, 0.0f,
+        0.8f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f 
+    };
+    GLuint vao, vbo;
+    gladLoadGL();
+    
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1Vertices), triangle1Vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.3f, 0.3f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
 
-	// Tell GLFW which version of openGL we are using
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
-	// Create a window object
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Window fails to load" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	// Make window the current context
-	glfwMakeContextCurrent(window);
+        glBindVertexArray(0);
 
 
-	// Load GLAD(they know the address of openGL functions)
-	gladLoadGL();
-	glViewport(0, 0, 800, 800);
+        glfwSwapBuffers(window);
+    }
 
-	// First, create shader program
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	// Next create VBO, VAO to load store data
-	
-	// Create vertices data
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
-	};
-
-
-	// Setting up VAO and VBO
-	// First create VAO and bind it
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	// Now create VBO, bind it to GL_ARRAY_BUFFER, send in the data
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// Now link VAO to VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// Now unbind
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// Set up color buffer
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-	// Termiante GLFW
-	glfwTerminate();
-	return 0;
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    
+    glfwTerminate();
+    
+    return 0;
 }
---
+++ 6_textures/YoutubeOpenGL/textureClass.h
@@ -0,0 +1,14 @@
+#ifndef TEXTURE_CLASS_H
+#define TEXTURE_CLAS_H
+
+#include<glad/glad.h>
+#include<string>
+#include<stb/stb_image.h>
+
+class Texture
+{
+public:
+	GLuint ID;
+
+};
+#endif
